14ea4c6c8a0e3628af40c9ea337a06a647414791
edeleonmatrix <edeleon@matrixformedia.com>
2022-11-26 00:16:45 -0600

    Added endpoint to create new user

--- CoderFinalProject_Emilio_De_Leon/Controllers/UserController.cs
+++ CoderFinalProject_Emilio_De_Leon/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using CoderFinalProject_Emilio_De_Leon.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text.Json;
 
@@ -60,6 +61,37 @@ namespace CoderFinalProject_Emilio_De_Leon.Controllers
                 }
             }
 
+        }
+
+        [EnableCors("AllowAnyOrigin")]
+        [HttpPost]
+        [Route("createuser")]
+        public dynamic CreateUser(User user)
+        {
+            String connectionString = "Server=sql.bsite.net\\MSSQL2016;Database=mammary0743_coderdb;User Id=mammary0743_coderdb;Password=2XuMoYCSjd5oVZ;\r\n";
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    using (SqlCommand command = new SqlCommand("INSERT INTO Usuario VALUES (@nombre, @apellido, @nombreUsuario, @contrasena, @mail)", connection))
+                    {
+                        connection.Open();
+                        command.Parameters.Add(new SqlParameter("nombre", SqlDbType.VarChar) { Value = user.Nombre });
+                        command.Parameters.Add(new SqlParameter("apellido", SqlDbType.VarChar) { Value = user.Apellido });
+                        command.Parameters.Add(new SqlParameter("nombreUsuario", SqlDbType.VarChar) { Value = user.NombreUsuario });
+                        command.Parameters.Add(new SqlParameter("contrasena", SqlDbType.VarChar) { Value = user.Contrasena });
+                        command.Parameters.Add(new SqlParameter("mail", SqlDbType.VarChar) { Value = user.Mail });
+
+                        var InsertNewUser = command.ExecuteNonQuery();
+                        return InsertNewUser;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
         }
     }
 }
